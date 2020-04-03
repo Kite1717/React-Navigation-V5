439ff21760dbf5411ac5a91b8988ef1c1ffd581c
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-04-04 00:06:48 +0300

    Create stack nav and Use multi nav

--- ReactNavigationApp/App.js
+++ ReactNavigationApp/App.js
@@ -1,21 +1,46 @@
 import * as React from 'react';
-import {StyleSheet,Text, View,SafeAreaView,Image} from 'react-native';
+import {StyleSheet,Text, View,SafeAreaView,Image,TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+//Handle Stack Navigator
+import {createStackNavigator} from '@react-navigation/stack';
+
+
 
 //create custom header
-function CustomHeader({title}) { // I sent the title as a props
+function CustomHeader({title,isHome,navigation}) { // I sent the title as a props
   return(
+
+    //****boolean default value false****
+
     <View style = {styles.customHeaderContainer}>
       <View style = {[styles.cellContainer , {flex : 1}]}>
-        <Image
-         style = {styles.icon}
-         source = {require('./src/assets/icons/menu.png')}
-         resizeMode = 'contain'
-        />
-      </View>
+      {
+        // handle button back
+        // header will appear on the main page
+        // otherwise the back button will appear
+        isHome ?
+            <Image
+              style = {styles.icon}
+              source = {require('./src/assets/icons/menu.png')}
+              resizeMode = 'contain'
+            />
+          :
+          <TouchableOpacity
+            style ={styles.backButtonContainer}
+            onPress = {() => navigation.goBack()}
+          >
+            <Image
+              style = {styles.icon}
+              source = {require('./src/assets/icons/back.png')}
+              resizeMode = 'contain'
+            />
+            <Text style ={{marginLeft: 5}}>Back</Text>
+          </TouchableOpacity>
+      }
 
+      </View>
       <View style = {[styles.cellContainer , {flex : 1.5}]}>
         <Text style ={styles.text}>{title}</Text>
       </View>
@@ -25,38 +50,112 @@ function CustomHeader({title}) { // I sent the title as a props
 
 }
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <CustomHeader title={'Home'}/>
+      <CustomHeader title={'Home'} isHome={true}/>
       <View style = {styles.textContainer}>
         <Text>Home!</Text>
+        {/* create screen detail*/}
+        <TouchableOpacity
+         style = {styles.detailButton}
+          // handle go to detail (Do not forget how you registered in which name parameter) name = 'HomeDetail'
+         onPress = { () => {navigation.navigate('HomeDetail')}}
+        >
+          <Text>Go Home detail</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
 
-function SettingsScreen() {
+function HomeScreenDetail({navigation}) { // navigation for goBack method
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <CustomHeader title={'Settings'}/>
+      <CustomHeader title={'Home Detail'} navigation={navigation} />
       <View style = {styles.textContainer}>
-        <Text>Settings</Text>
+        <Text>Home detail :)</Text>
       </View>
     </SafeAreaView>
   );
 }
 
+function SettingsScreen({navigation}) {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader title={'Setting'} isHome={true}/>
+      <View style = {styles.textContainer}>
+        <Text>Settings!</Text>
+        <TouchableOpacity
+          onPress = { () => {navigation.navigate('SettingDetail')}}
+          style = {styles.detailButton}
+        >
+          <Text>Go Setting detail</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+
+function SettingsScreenDetail({navigation}) { // navigation for goBack method
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader title={'Setting Detail'} navigation={navigation}/>
+      <View style = {styles.textContainer}>
+        <Text>Setting Detail :)</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+//NAVS
 const Tab = createBottomTabNavigator();
+// separate stack should be produced for each page
+const StackHome = createStackNavigator();
+const StackSetting = createStackNavigator();
+//navigation settings
+// Must be added to all components in the stack
+//to destroy the default header
+const navOptionHandler = () => ({
+
+  headerShown : false,  // we closed the default header
+});
+
+
+//create HomeStack
+function HomeStack() {
+  return(
+    <StackHome.Navigator initialRouteName = 'Home'>
+      <StackHome.Screen  name='Home' component = {HomeScreen} options={navOptionHandler}/>
+      <StackHome.Screen  name='HomeDetail' component = {HomeScreenDetail} options={navOptionHandler}/>
+    </StackHome.Navigator>
+  )
+}
+
+//create SettingStack
+function SettingStack() {
+  return(
+    <StackSetting.Navigator initialRouteName = 'Setting'>
+      <StackSetting.Screen  name='Setting' component = {SettingsScreen} options={navOptionHandler}/>
+      <StackSetting.Screen  name='SettingDetail' component = {SettingsScreenDetail} options={navOptionHandler}/>
+    </StackSetting.Navigator>
+  )
+}
 
 //Minimal example of tab-based navigation
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
 
+        {/*now we will put the stacks we created
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+
+        {/* we have two header these are default header from StackNavigation and my custom header */}
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Settings" component={SettingStack} />
 
       </Tab.Navigator>
     </NavigationContainer>
@@ -96,6 +195,15 @@ const styles = StyleSheet.create({
     width : 20,
     height:  20,
     marginLeft : 5,
+
+  },
+  detailButton : {
+
+    marginTop : 20,
+  },
+  backButtonContainer : {
+    flexDirection: 'row',
+    alignItems:  'center',
 
   }
 
