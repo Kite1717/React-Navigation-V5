184cb6e7e433c4f1fef7117990b61035c39987c6
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-04-24 14:28:24 +0300

    Fixed state

--- ReactNavigationApp/src/drawer/NotificationsScreen.js
+++ ReactNavigationApp/src/drawer/NotificationsScreen.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 import {CustomHeader} from '../index';
+import styles from '../styles/style';
 
-export default class NotificationsScreen extends Component {
+export  class NotificationsScreen extends Component {
   render() {
     return (
 			<SafeAreaView style={{ flex: 1 }}>
-				<CustomHeader title={'Notification'} navigation={navigation}/>
+				<CustomHeader title={'Notification'} navigation={this.props.navigation}/>
 				<View style = {styles.textContainer}>
 					<Text>Notification Screen !</Text>
 				</View>
