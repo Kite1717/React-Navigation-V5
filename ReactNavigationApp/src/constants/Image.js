5c1799992fe5a157875a0d08e16cfbe1954aae8a
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-04-24 14:19:30 +0300

    Create const for images path

---
+++ ReactNavigationApp/src/constants/Image.js
@@ -0,0 +1,14 @@
+const IMAGE = {
+
+	ICON_PROFILE : require('../assets/icons/profile.png'),
+	ICON_MENU : require('../assets/icons/menu.png'),
+	ICON_BACK : require('../assets/icons/back.png'),
+	ICON_HOME_WHITE : require('../assets/icons/home-white.png'),
+	ICON_HOME_BLACK : require('../assets/icons/home-black.png'),
+	ICON_SETTING_WHITE : require('../assets/icons/settings-white.png'),
+	ICON_SETTING_BLACK : require('../assets/icons/settings-black.png'),
+
+
+}
+
+export {IMAGE};
